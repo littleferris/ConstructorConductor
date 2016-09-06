8cc7eef8e098c563f035768b73be86eb5b58ed66
Mark Ferris <littleferris@gmail.com>
2016-09-05 21:27:04 -0600

    Working on it

--- instanceArray.js
+++ instanceArray.js
@@ -1,5 +1,5 @@
 /*
-  Your 3 Users will be the following. 
+  Your 3 Users will be the following.
     0) Tyler, tylermcginnis33@gmail.com, 'iLoveJS'
     1) Cahlan, cahlan@devmounta.in, 'iLoveHashtags'
     2) Lenny, lenny@theLenster.com, 'iLoveLentilSoup'
@@ -13,19 +13,24 @@ var User = function(name, email, pw){
 
 //Create an Array called 'users' that will store all our instances of User.
 
-  //code here
+  var users = [];
+
+  var tyler = new User('Tyler', 'tylermcginnis33@gmail.com', 'iLoveJS');
+  var cahlan = new User('Cahlan', 'cahlan@devmounta.in', 'iLoveHashtags');
+  var lenny = new User('Lenny', 'lenny@theLenster.com', 'iLoveLentilSoup');
 
 
 //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
 
   //code here
+  users.push(tyler, cahlan, lenny, mark);
 
-console.log('Tyler\'s information is ');
+console.log('Tyler\'s information is ', tyler);
 //Console.log all of Tylers information
 
   //code here
 
-console.log('Lenny\'s information is ');
+console.log('Lenny\'s information is ' lenny);
 //Now console.log all of Lennys information
 
   //code here
@@ -34,8 +39,12 @@ console.log('Lenny\'s information is ');
 //Now create another instance of User using your own information and then add that to your users array.
 
   //code here
+  var mark = new User('Mark', 'someemail@email', 'iLoveBacon');
+
+
+
 
 console.log('All my users names are ');
-//Now loop through your users Array and console.log every users name. 
+//Now loop through your users Array and console.log every users name.
 
   //code here
--- sayName.js
+++ sayName.js
@@ -1,13 +1,24 @@
 //Create a Person constructor that accepts name and age as parameters and sets those properties accordingly in the Constructor.
 
-  //code here
+  var Person = function(name, age) {
+    this.name = name;
+    this.age = age;
+  }//code here
 
 
 //Now create three instances of Person with data you make up
 
-  //code here
+  var mark = new Person('Mark', 24);
+  var miranda = new Person('Miranda', 22);
+  var daren = new Person('Daren', 24);//code here
 
 
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
   //code here
+Person.prototype = {
+  getName: function() {
+    alert(this.name);
+  }
+}
+mark.getName();
